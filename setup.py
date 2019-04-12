e3cc226e4e28183d7cbe5ceb42f3d812006f82d5
Nache <nache.nache@gmail.com>
2019-04-12 23:31:14 +0200

    setup version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(HERE, 'README.md')).read()
 
 setup(
     name='django-country2',
-    version='0.0.2',
+    version='0.0.4',
     url='https://github.com/Develatio/django-country2',
     author='Develatio Technologies S.L.',
     author_email='contacto@develat.io',
