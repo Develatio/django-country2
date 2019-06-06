21d8b2bfef827da919639f25d7d955dc363394e3
Sergio <sergiormb88@gmail.com>
2019-06-06 12:38:46 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
     url='https://github.com/Develatio/django-country2',
     author='Develatio Technologies S.L.',
     author_email='contacto@develat.io',
-    description=('Provides Django middleware that detects '
-                 'a country the request came from.'),
+    description=('Provides Django middleware that detects a country the request came from.'),
     long_description=README,
+    long_description_content_type="text/markdown",
     packages=['django_country2'],
     include_package_data=True,
     scripts=[],
